3086932db46be4abc4b42b14f291058ecfb0ef3f
pavanchaudhari <44545671+pavanchaudhari@users.noreply.github.com>
2018-10-28 20:11:26 +0530

    first commit
    Add base CV

---
+++ precache-manifest.0337d00ec61a2b1e55f95a6c41719e4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc86c8ac497f917bf6fc",
+    "url": "/static/js/main.bc86c8ac.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "bc86c8ac497f917bf6fc",
+    "url": "/static/css/main.b0fc0bd5.chunk.css"
+  },
+  {
+    "revision": "0103a80ac2d8e136589a13714974332f",
+    "url": "/index.html"
+  }
+];
