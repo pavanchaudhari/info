fa193ba844c3e5c851a599c57b783a4e09d970ad
Pawan Choudhari <P10493049@capita.co.uk>
2020-01-12 15:34:21 +0530

    Update Info

---
+++ precache-manifest.4914f64eacc7a1f7aed8f5bb84be7418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db823faced1e60495cb5bcfb26a1ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f735af269c48bcc92651",
+    "url": "/static/css/main.62cd1525.chunk.css"
+  },
+  {
+    "revision": "47239f4b82ee6ffaa965",
+    "url": "/static/js/2.a73654ef.chunk.js"
+  },
+  {
+    "revision": "fd238ebb69e2e976c69a9dc63b5afb8d",
+    "url": "/static/js/2.a73654ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f735af269c48bcc92651",
+    "url": "/static/js/main.9072f4ce.chunk.js"
+  },
+  {
+    "revision": "c8d7855bb8c34e28ab24",
+    "url": "/static/js/runtime-main.36661ff0.js"
+  }
+]);
